c0dfeeb3af56005eb38e65f70bff18bc2ff95d35
Richard Ward <richard@richard.ward.name>
2016-12-04 12:30:47 +0000

    neaten up somewhat

--- src/day_1.rs
+++ src/day_1.rs
@@ -5,76 +5,31 @@ use std::io::{Read};
 use self::core::str::FromStr;
 
 pub fn part_1() -> i64 {
-    let directions = read_file("inputs/1-1.txt").trim().split(", ").map(|str| {
-        let direction: Relative = str[0..1].parse().unwrap();
-        let distance: i64 = str[1..].parse().unwrap();
-        (direction, distance)
-    }).collect();
-    return taxi_abs(directions);
-}
-
-#[derive(Clone, Copy)]
-enum Relative {
-    Left,
-    Right,
-}
-
-impl FromStr for Relative {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "L" => Ok(Relative::Left),
-            "R" => Ok(Relative::Right),
-            _ => Err("Unknown relative direction ".to_string() + s),
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-enum Absolute {
-    North,
-    East,
-    South,
-    West,
-}
-
-impl Absolute {
-    fn rotate(self, rel: Relative) -> Absolute {
-        match rel {
-            Relative::Right => match self {
-                Absolute::North => Absolute::East,
-                Absolute::East => Absolute::South,
-                Absolute::South => Absolute::West,
-                Absolute::West => Absolute::North,
-            },
-            Relative::Left => match self {
-                Absolute::East => Absolute::North,
-                Absolute::South => Absolute::East,
-                Absolute::West => Absolute::South,
-                Absolute::North => Absolute::West,
-            },
-        }
-    }
-
-    fn vector(self, distance: i64) -> (i64, i64) {
-        match self {
-            Absolute::North => (distance, 0),
-            Absolute::East => (0, distance),
-            Absolute::South => (-distance, 0),
-            Absolute::West => (0, -distance),
-        }
-    }
+    return taxi_abs(load_instructions());
 }
 
 
-fn taxi_abs(directions: Vec<(Relative, i64)>) -> i64 {
-    let mut facing = Absolute::North;
+fn load_instructions() -> Vec<Instruction> {
+    read_file("inputs/1-1.txt")
+        .trim()
+        .split(", ")
+        .map(parse_instruction)
+        .collect()
+}
 
-    let (north, east) = directions
-        .into_iter()
+fn parse_instruction(str: &str) -> Instruction {
+    (str[0..1].parse::<RelativeDirection>().unwrap(), str[1..].parse::<i64>().unwrap())
+}
+
+type Instruction = (RelativeDirection, i64);
+
+fn taxi_abs(directions: Vec<Instruction>) -> i64 {
+    let mut facing = AbsoluteDirection::North;
+
+    let (north, east) = directions.into_iter()
         .map(|(relative_direction, distance)| {
             facing = facing.rotate(relative_direction);
-            facing.vector(distance)
+            facing.as_vector(distance)
         })
         .fold((0, 0), |(n1, e1), (n2, e2)| (n1 + n2, e1 + e2));
 
@@ -88,25 +43,77 @@ fn read_file(file_name: &str) -> String {
     data
 }
 
+#[derive(Clone, Copy)]
+enum RelativeDirection {
+    Left,
+    Right,
+}
+
+impl FromStr for RelativeDirection {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "L" => Ok(RelativeDirection::Left),
+            "R" => Ok(RelativeDirection::Right),
+            _ => Err("Unknown relative direction ".to_string() + s),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum AbsoluteDirection {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl AbsoluteDirection {
+    fn rotate(self, rel: RelativeDirection) -> AbsoluteDirection {
+        match rel {
+            RelativeDirection::Right => match self {
+                AbsoluteDirection::North => AbsoluteDirection::East,
+                AbsoluteDirection::East => AbsoluteDirection::South,
+                AbsoluteDirection::South => AbsoluteDirection::West,
+                AbsoluteDirection::West => AbsoluteDirection::North,
+            },
+            RelativeDirection::Left => match self {
+                AbsoluteDirection::East => AbsoluteDirection::North,
+                AbsoluteDirection::South => AbsoluteDirection::East,
+                AbsoluteDirection::West => AbsoluteDirection::South,
+                AbsoluteDirection::North => AbsoluteDirection::West,
+            },
+        }
+    }
+
+    fn as_vector(self, distance: i64) -> (i64, i64) {
+        match self {
+            AbsoluteDirection::North => (distance, 0),
+            AbsoluteDirection::East => (0, distance),
+            AbsoluteDirection::South => (-distance, 0),
+            AbsoluteDirection::West => (0, -distance),
+        }
+    }
+}
 
 #[test]
 fn taxicab_abs_solves_the_day_1_examples() {
     assert_eq!(5, taxi_abs(vec!(
-        (Relative::Right, 2),
-        (Relative::Left, 3),
+        (RelativeDirection::Right, 2),
+        (RelativeDirection::Left, 3),
     )));
 
     assert_eq!(2, taxi_abs(vec!(
-        (Relative::Right, 2),
-        (Relative::Right, 2),
-        (Relative::Right, 2),
+        (RelativeDirection::Right, 2),
+        (RelativeDirection::Right, 2),
+        (RelativeDirection::Right, 2),
     )));
 
     assert_eq!(12, taxi_abs(vec!(
-        (Relative::Right, 5),
-        (Relative::Left, 5),
-        (Relative::Right, 5),
-        (Relative::Right, 3),
+        (RelativeDirection::Right, 5),
+        (RelativeDirection::Left, 5),
+        (RelativeDirection::Right, 5),
+        (RelativeDirection::Right, 3),
     )));
 }
 
